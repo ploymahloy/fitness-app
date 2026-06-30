90ed9f9be9c9c06e5fcb6c706b51b913b0fad64b
ploymahloy <patrick.mahloy@gmail.com>
2026-06-30 11:06:20 -0400

    Stand up api server
    Add dependencies for api server, build healthcheck endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Project init");
+use axum::{
+    Router, 
+    Json,
+    routing::get, 
+    response::IntoResponse
+};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/health", get(health_check_handler));
+
+    println!("Server started successfully at 0.0.0.0:8080");
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn health_check_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "API Services";
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
 }
